c1c065ab217a003f4820745480e83d974bc16dc2
Andres <andrespaez123@hotmail.com>
2023-10-20 08:55:58 -0500

    first commit

---
+++ NetCoreYouTube/Controllers/UsuarioController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using NetCoreJWT.Models;
+using Newtonsoft.Json;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace NetCoreJWT.Controllers
+{
+    [ApiController]
+    [Route("usuario")]
+    public class UsuarioController : Controller
+    {
+        public IConfiguration _configuration { get; set; }
+        public UsuarioController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public dynamic IniciarSesion([FromBody] Object optData)
+        {
+            var data = JsonConvert.DeserializeObject<dynamic>(optData.ToString());
+
+            string user = data.usuario.ToString();
+            string password = data.password.ToString();
+
+            Usuario usuario = Usuario.DB().Where(x => x.usuario == user && x.password == password).FirstOrDefault();
+
+            if (usuario == null)
+            {
+                return new
+                {
+                    succes = false,
+                    message = "credenciales incorrectas",
+                    result = ""
+                };
+            }
+            var jwt = _configuration.GetSection("Jwt").Get<Jwt>();
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, jwt.Subject),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()),
+                new Claim("id", usuario.idUsuario),
+                new Claim("usuario", usuario.usuario),
+
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwt.Key));
+            var singIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                jwt.Issuer,
+                jwt.Audience,
+                claims,
+                expires: DateTime.Now.AddMinutes(4),
+                signingCredentials: singIn
+                );
+
+            return new
+            {
+                success = true,
+                message = "exito",
+                result = new JwtSecurityTokenHandler().WriteToken(token)
+
+            };
+
+        }
+    }
+}
---
+++ NetCoreYouTube/Models/Usuario.cs
@@ -0,0 +1,48 @@
+﻿namespace NetCoreJWT.Models
+{
+    public class Usuario
+    {
+        public string? idUsuario { get; set; }
+        public string? usuario { get; set; }
+        public string? password { get; set; }
+        public string? rol { get; set; }
+
+        public static List<Usuario> DB()
+        {
+            var list = new List<Usuario>() {
+                        new Usuario
+            {
+                idUsuario = "1",
+                usuario = "mateo",
+                password = "password",
+                rol = "empleado"
+            },
+             new Usuario
+             {
+                 idUsuario = "2",
+                 usuario = "lucas",
+                 password = "password",
+                 rol = "administrador"
+             },
+                  new Usuario
+                  {
+                      idUsuario = "3",
+                      usuario = "juan",
+                      password = "password",
+                      rol = "asesor"
+                  },
+                     new Usuario
+                  {
+                      idUsuario = "4",
+                      usuario = "marcos",
+                      password = "password",
+                      rol = "empleado"
+                  }
+        };
+
+            return list;
+
+
+        }
+    }
+}
